1fb7471a8b1cf1c5eb137c74ce7ac55417ae13e0
maitysubhasis <hola.maity@gmail.com>
2024-12-12 23:17:12 +0530

    add project store provider

---
+++ internal/store/provider.go
@@ -0,0 +1,39 @@
+package store
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+var ErrStoreNotFound = errors.New("store not found")
+
+type StoreProvider interface {
+	Provide(projectID uuid.UUID) (Store, error)
+}
+
+type ProjectStoreProvider struct {
+	stores map[string]Store
+}
+
+func NewProjectStoreProvider() *ProjectStoreProvider {
+	return &ProjectStoreProvider{
+		stores: make(map[string]Store),
+	}
+}
+
+func (p *ProjectStoreProvider) Provide(projectID uuid.UUID) (Store, error) {
+	if store, ok := p.stores[projectID.String()]; ok {
+		return store, nil
+	}
+
+	return nil, ErrStoreNotFound
+}
+
+type DefaultProvider struct {
+	store Store
+}
+
+func (p *DefaultProvider) Provide(projectID uuid.UUID) (Store, error) {
+	return p.store, nil
+}
